97cc24815dbc3bfd6a4c8ca1614fc751394cadca
Gustavo <erplogistic0@gmail.com>
2026-05-05 01:20:44 -0500

    Fix config SMTP Gmail

--- mailer.js
+++ mailer.js
@@ -4,7 +4,9 @@ console.log('MAIL_USER:', process.env.MAIL_USER);
 console.log('MAIL_PASS existe:', !!process.env.MAIL_PASS);
 
 const transporter = nodemailer.createTransport({
-  service: 'gmail',
+  host: 'smtp.gmail.com',
+  port: 465,
+  secure: true,
   auth: {
     user: process.env.MAIL_USER,
     pass: process.env.MAIL_PASS,
